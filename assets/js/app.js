d19c18f280389e4647caef594718e093111856c0
EricKoh <EricKohx@gmail.com>
2015-07-09 14:13:46 +0800

    update background

--- assets/js/app.js
+++ assets/js/app.js
@@ -51,30 +51,53 @@ require(['jquery', 'ScrollMagic', 'TweenMax', 'animationVelocity', 'indicators',
   $(function() {
   	 var controller = new ScrollMagic.Controller();
 
-  	 var scene = new ScrollMagic.Scene({triggerElement: "#feature-box"})
+  	/* var scene = new ScrollMagic.Scene({triggerElement: "#feature-box"})
 			// trigger a velocity opaticy animation
 			.setVelocity(".feature-box", {opacity: 1}, {duration: 1000})
 			.addIndicators() // add indicators (requires plugin)
-			.addTo(controller);
+			.addTo(controller);*/
 
 
-		var scene = new ScrollMagic.Scene({triggerElement: "#trigger-feature-title"})
+		var scene = new ScrollMagic.Scene({triggerElement: "#trigger-what-we-do"})
 			// trigger a velocity opaticy animation
 			.setVelocity(".site-section-title.feature", {opacity: 1}, {duration: 1300})
 			// .setVelocity(".site-section-title.feature-sub", {opacity: 1}, {duration: 1500})
 			.addIndicators() // add indicators (requires plugin)
 			.addTo(controller);
 
-		var scene = new ScrollMagic.Scene({triggerElement: "#trigger-feature-title"})
+/*		var scene = new ScrollMagic.Scene({triggerElement: "#trigger-feature-title"})
 			// trigger a velocity opaticy animation
 			.setVelocity(".site-section-title.feature-sub", {opacity: 1}, {duration: 1500})
 			.addIndicators() // add indicators (requires plugin)
-			.addTo(controller);
+			.addTo(controller);*/
 
-		var scene = new ScrollMagic.Scene({triggerElement: "#trigger-feature-content"})
+		/*var scene = new ScrollMagic.Scene({triggerElement: "#trigger-feature-content"})
 			// trigger a velocity opaticy animation
 			.setVelocity(".intro-wrapper", {opacity: 1}, {duration: 1800})
 			.addIndicators() // add indicators (requires plugin)
+			.addTo(controller);*/
+
+		var scene = new ScrollMagic.Scene({triggerElement: "#trigger-navbar"})
+			// trigger a velocity opaticy animation
+			.setVelocity(".site-navbar", {backgroundColor: '#000', backgroundColorAlpha: 0.85}, {duration: 800})
+			.addIndicators() // add indicators (requires plugin)
 			.addTo(controller);
+
+
+
+		function scrollToElement(selector, time, verticalOffset) {
+	    time = typeof(time) != 'undefined' ? time : 1000;
+	    verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
+	    element = $(selector);
+	    offset = element.offset();
+	    offsetTop = offset.top + verticalOffset;
+	    $('html, body').animate({
+	        scrollTop: offsetTop
+	    }, time);          
+		}
+
+		$('.site-arrow-down').click(function() {
+	    scrollToElement('#intro-tools');
+		});
   });
 });
