4556de4edbeb8fc74d133fcc4258b363de6bbb42
EricKoh <EricKohx@gmail.com>
2015-07-09 18:58:46 +0800

    fixes padding problem

--- assets/js/app.js
+++ assets/js/app.js
@@ -60,14 +60,14 @@ require(['jquery', 'ScrollMagic', 'TweenMax', 'animationVelocity', 'indicators',
 
 		var scene = new ScrollMagic.Scene({triggerElement: "#trigger-intro"})
 			// trigger a velocity opaticy animation
-			.setVelocity(".site-section-title.feature", {opacity: 1}, {duration: 1300})
+			.setVelocity(".site-section-title.intro", {opacity: 1}, {duration: 1300})
 			// .setVelocity(".site-section-title.feature-sub", {opacity: 1}, {duration: 1500})
 			.addIndicators() // add indicators (requires plugin)
 			.addTo(controller);
 
 		var scene = new ScrollMagic.Scene({triggerElement: "#trigger-intro"})
 			// trigger a velocity opaticy animation
-			.setVelocity(".site-section-title.feature-sub", {opacity: 1}, {duration: 1500})
+			.setVelocity(".site-section-title.intro-sub", {opacity: 1}, {duration: 1500})
 			.addIndicators() // add indicators (requires plugin)
 			.addTo(controller);
 
